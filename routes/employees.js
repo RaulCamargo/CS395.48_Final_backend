d0ddb7bdb5add99aa3c5a9d867ba73f931c6c50b
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-18 02:58:27 -0500

    Added edit employee by id route.

--- routes/employees.js
+++ routes/employees.js
@@ -19,6 +19,18 @@ router.get('/:id', ash(async(req, res) =>
         let employee = await Employee.findByPk(req.params.id, {include: [Task]});
         res.status(200).json(employee);
     })
-    );
+);
+
+// Edit employee
+router.put('/:id', ash(async(req, res) =>
+    {
+        await Employee.update(req.body, 
+            {
+                where: { id: req.params.id}
+            });
+        let employee = await Employee.findByPk(req.params.id, {include: [Task]});
+        res.status(200).json(employee);
+    })
+);
 
 module.exports = router;
