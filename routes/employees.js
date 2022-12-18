34c33dcbe31bdfc6bafc9718f3a635ab53713584
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-18 02:44:57 -0500

    Return ALL employees route added

--- routes/employees.js
+++ routes/employees.js
@@ -2,4 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {Task, Employee} = require('../database/models');
 
+// async handler
+const ash = require('express-async-handler');
+
+// Get ALL employees
+router.get('/', ash(async(req, res) =>
+    {
+        let employees = await Employee.findAll({include: [Task]});
+        res.status(200).json(employees);
+    })
+);
+
 module.exports = router;
